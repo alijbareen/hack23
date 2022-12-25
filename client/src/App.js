218a1f329e68c9eecd0b6f45ab084d44c7b72b3b
alijbareen <alijb@post.bgu.ac.il>
2022-12-26 00:21:00 +0200

    1

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>test</p>
         <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
